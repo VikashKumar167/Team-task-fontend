537ef665194f5727d7e1d88374d336d8d0dd8487
Vikash Kumar <firsttime1567@gmail.com>
2026-05-10 21:53:52 +0530

    connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://team-task-backend-1.onrender.com/",
 });
 
 // Request Interceptor
